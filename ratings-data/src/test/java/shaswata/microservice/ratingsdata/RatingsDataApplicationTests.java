624f036a95f9c92a1eaed62da4d3a09b59e2336d
Shaswata79 <57040344+Shaswata79@users.noreply.github.com>
2021-07-01 14:15:22 +0600

    Initialized 3 spring boot projects

---
+++ ratings-data/src/test/java/shaswata/microservice/ratingsdata/RatingsDataApplicationTests.java
@@ -0,0 +1,13 @@
+package shaswata.microservice.ratingsdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatingsDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
