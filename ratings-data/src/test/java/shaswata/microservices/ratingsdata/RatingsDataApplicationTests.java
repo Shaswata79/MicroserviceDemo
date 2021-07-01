c50fcb49540f3aa8c9855ddba01b7cc3a264545b
Shaswata79 <shaswatabhatt2030@gmail.com>
2021-07-01 14:28:50 +0600

    Fixed Gradle issue

---
+++ ratings-data/src/test/java/shaswata/microservices/ratingsdata/RatingsDataApplicationTests.java
@@ -0,0 +1,13 @@
+package shaswata.microservices.ratingsdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatingsDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
